a624b9647f347dec0a0af17ea7dff6b72eaadad0
LeVanHang <vanhang1802@gmail.com>
2022-10-11 17:14:14 +0700

    array

---
+++ src/arraylish/Array.java
@@ -0,0 +1,22 @@
+package arraylish;
+
+import java.util.ArrayList;
+
+public class Array {
+    public static void main(String[] args) {
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add("pizza");
+        food.add("hambuger");
+        food.add("hotdog");
+
+        //food.set(0,"sushi");
+        //food.remove(2);
+        food.clear();
+
+        for (int i = 0; i < food.size(); i++) {
+            System.out.println(food.get(i));
+
+        }
+    }
+}
